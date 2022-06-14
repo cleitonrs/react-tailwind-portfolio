021af875c1016afe252720c4a1cd9e4942db2d9f
cleitonrs <rscleitonrs@gmail.com>
2022-06-14 13:20:55 -0300

    dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,44 @@
 import React from "react"
+import { useEffect } from "react"
+import { useState } from "react"
 import HeroSection from "./components/HeroSection"
 
 function App() {
+  const [theme, setTheme] = useState(null)
+
+  useEffect(() => {
+    if(window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      setTheme('dark')
+    } else {
+      setTheme('light')
+    }
+  }, [])
+
+  useEffect(() => {
+    if(theme === 'dark') {
+      document.documentElement.classList.add('dark')
+    } else {
+      document.documentElement.classList.remove('dark')
+    }
+  }, [theme])
+
+  const handleThemeSwitch = () => {
+    setTheme(theme === 'dark' ? 'light' : 'dark')
+  }
   
   return (
-    <div className='font-inter'>
-      <HeroSection />
-    </div>
+    <>
+      <button
+       type="button"
+       onClick={handleThemeSwitch}
+       className="fixed z-10 right-2 top-2 bg-indigo-500 text-lg p-1 rounded-md  " 
+      >
+        {theme === 'dark' ? '🌙' : '🌞'}
+      </button>
+      <div className='font-inter bg:white dark:bg-slate-900'>
+        <HeroSection />
+      </div>
+    </>
   )
 }
 
