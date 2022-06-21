5489c8b03777087304c92cc9bd0ae558e5555959
cleitonrs <rscleitonrs@gmail.com>
2022-06-21 08:45:18 -0300

    fixes

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -6,7 +6,7 @@ import ServiceItem from './ServiceItem'
 const Services = () => {
   return (
     <div className='py-12'>
-      <SectionTitle>Our Services</SectionTitle>
+      <SectionTitle>My Services</SectionTitle>
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
         {services.map(service => (
           <ServiceItem 
